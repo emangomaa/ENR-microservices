8ddd05d5b4a0d68ee3a63f90e80397655e55f12d
eman gomaa <emangomaa149@gmail.com>
2026-08-09 00:22:43 +0300

    set jwt infrastructure

---
+++ libs/common/src/jwt/index.ts
@@ -0,0 +1,4 @@
+export * from './jwt.service';
+export * from './jwt.module';   
+export * from './interfaces/jwt-payload.interface';
+export * from './jwt.constants';
---
+++ libs/common/src/jwt/jwt.service.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService as NestJwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { UnauthenticatedException } from '../exceptions';
+
+
+@Injectable()
+export class JwtService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: NestJwtService,
+  ) {}
+
+  async sign(payload: JwtPayload){
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_EXPIRES_IN') || '30m',
+    });
+    
+  }
+
+  verify(token: string): JwtPayload {
+    try {
+      return this.jwtService.verify<JwtPayload>(
+        token,
+        {
+          secret: this.configService.get<string>('JWT_SECRET'),
+        },
+      );
+    } catch {
+      throw new UnauthenticatedException();
+    }
+  }
+}
